dc9a77150eee9b392298987e7014df4e1bf48d40
ketwalters <ketwalters@gmail.com>
2016-01-26 15:30:35 -0500

    add precompike to application.rb

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module StyleSearch5
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile << "*.css"
     config.assets.initialize_on_precompile = false
 
   end
